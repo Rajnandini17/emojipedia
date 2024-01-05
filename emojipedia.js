dc9ebf2bc6a6475fb98a03c20da34965372de32f
Rajnandini Singh <85100248+Rajnandini17@users.noreply.github.com>
2024-01-05 17:08:05 +0530

    add emoji data

---
+++ emojipedia.js
@@ -0,0 +1,67 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "😀",
+    name: "Grinning Face",
+    meaning:
+      "A yellow face with simple, open eyes and a broad, open smile, showing upper teeth and tongue on some platforms. Often conveys general pleasure and good cheer or humor.",
+  },
+  {
+    id: 5,
+    emoji: "😉",
+    name: "Winking Face",
+    meaning:
+      "A yellow face with a slight smile or open mouth shown winking, usually its left eye. May signal a joke, flirtation, hidden meaning, or general positivity. Tone varies, including playful, affectionate, suggestive, or ironic.",
+  },
+  {
+    id: 6,
+    emoji: "😏",
+    name: "Smirking Face",
+    meaning:
+      "A yellow face with a sly, smug, mischievous, or suggestive facial expression. It features a half-smile, raised eyebrows, and eyes looking to the side. Often used to convey flirtation or sexual innuendo.",
+  },
+  {
+    id: 7,
+    emoji: "🥰",
+    name: "Smiling Face with Hearts",
+    meaning:
+      "A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love. The placement of the hearts varies slightly by platform.",
+  },
+  {
+    id: 8,
+    emoji: "🫣",
+    name: "Face with Peeking Eye",
+    meaning:
+      "A yellow face with its hands over its eyes and raised eyebrows. One wide eye is peeking through the fingers, can be used to express the duality of wanting to look away from something because it's frightening, disgusting, or embarrassing, but not being able to.",
+  },
+  {
+    id: 9,
+    emoji: "🥺",
+    name: "Pleading Face",
+    meaning:
+      "A yellow face with furrowed eyebrows, a small frown, and large, “puppy dog” eyes, as if begging or pleading. May also represent adoration or feeling touched by a loving gesture.",
+  },
+];
+
+export default emojipedia;
